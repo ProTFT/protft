daa05ebfb986cc8b7f0536c194678244c60edb10
Lucas Heim <lucasheim1983@gmail.com>
2023-03-29 21:59:37 +0200

    infra: reduce memory :tada:

--- packages/infra/ecs_task.tf
+++ packages/infra/ecs_task.tf
@@ -2,8 +2,8 @@ resource "aws_ecs_task_definition" "ptft_ecs_task" {
   family                   = "ptft_task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 512
-  memory                   = 2048
+  cpu                      = 256
+  memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([{
